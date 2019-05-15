c10e894c05c169ad69b277846b75e3c348e84a5a
RJ Bowen <rjbowen97@gmail.com>
2019-05-14 22:24:37 -0400

    Adjacent cell counting is working!

--- main.py
+++ main.py
@@ -4,25 +4,62 @@ import time
 sense = SenseHat()
 
 black = [0, 0, 0]  # Red
-red = [255, 0, 0]  # Red
+off = black
 
-def getTotalActiveAdjacentCells(inputGrid, targetCell):
-    totalActiveAdjacentCells = 0
+red = [255, 0, 0]  # Red
+on = red
 
 grid = []
 
-for currentIndex in range(0, 8):
+for currentRowIndex in range(0, 8):
     currentRow = []
-    for currentIndex in range(0, 8):
+    for currentColumnIndex in range(0, 8):
         if random.randint(0, 1):
-            currentRow.append(red)
+            currentRow.append(on)
         else:
             currentRow.append(black)
     grid.append(currentRow)
 
 for currentInteration in range(0, 100):
     time.sleep(1)
+    for currentRowIndex in range(0, 8):
+        for currentColumnIndex in range(0, 8):
+            totalActiveAdjacentCells = 0
+
+            if currentRowIndex - 1 >= 0:
+                if currentColumnIndex - 1 >= 0:
+                    if grid[currentRowIndex - 1][currentColumnIndex - 1] == on:
+                        totalActiveAdjacentCells += 1
+
+                if grid[currentRowIndex - 1][currentColumnIndex] == on:
+                    totalActiveAdjacentCells += 1
+
+                if currentColumnIndex + 1 <= 7:
+                    if grid[currentRowIndex - 1][currentColumnIndex + 1] == on:
+                        totalActiveAdjacentCells += 1
+
+            if currentRowIndex + 1 <= 7:
+                if currentColumnIndex - 1 >= 0:
+                    if grid[currentRowIndex + 1][currentColumnIndex - 1] == on:
+                        totalActiveAdjacentCells += 1
+
+                if grid[currentRowIndex + 1][currentColumnIndex] == on:
+                    totalActiveAdjacentCells += 1
+
+                if currentColumnIndex + 1 <= 7:
+                    if grid[currentRowIndex + 1][currentColumnIndex + 1] == on:
+                        totalActiveAdjacentCells += 1
+
+            if currentColumnIndex - 1 >= 0:
+                if grid[currentRowIndex][currentColumnIndex - 1] == on:
+                    totalActiveAdjacentCells += 1
+            if currentColumnIndex + 1 <= 7:
+                if grid[currentRowIndex][currentColumnIndex + 1] == on:
+                    totalActiveAdjacentCells += 1
+
+            print(totalActiveAdjacentCells)
 
 
-displayGrid = [currentCell for currentSublist in grid for currentCell in currentSublist]
+displayGrid = [
+    currentCell for currentSublist in grid for currentCell in currentSublist]
 sense.set_pixels(displayGrid)
