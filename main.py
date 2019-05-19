48a3f50c39f7c831e1f6e0b17d2fee69c110ab90
RJ Bowen <rjbowen97@gmail.com>
2019-05-18 21:09:11 -0400

    Appears to be working!

--- main.py
+++ main.py
@@ -4,32 +4,65 @@ import time
 
 
 class SenseHatGOL:
+    livingCellColor = [0, 0, 0]
+    offColor = [0, 0, 0]
+
     def __init__(self, secondsPerIteration, iterationLimit):
         self.secondsPerIteration = secondsPerIteration
         self.iterationLimit = iterationLimit
+
         self.initSenseHat()
-        self.golGrid = GOLGrid()
+        self.initGOLGrid()
 
     def initSenseHat(self):
         self.sense = SenseHat()
-        self.sense.low_light = True
+        self.sense.low_light = False
+
+    def initGOLGrid(self):
+        self.golGrid = GOLGrid()
+        self.golGrid.randomizeGrid()
+        self.changelivingCellColor()
 
     def execute(self):
         currentIteration = 0
         while True:
+            hasLivingCells = False
+            displayGrid = []
+            for currentSublist in self.golGrid.grid:
+                for currentCell in currentSublist:
+                    if currentCell:
+                        displayGrid.append(self.livingCellColor)
+                        hasLivingCells = True
+                    else:
+                        displayGrid.append(self.offColor)
+
+            currentIteration += 1
+            self.sense.set_pixels(displayGrid)
+
+            if (currentIteration == self.iterationLimit or (not hasLivingCells) or self.golGrid.isStagnant):
+                currentIteration = 0
+                self.golGrid.randomizeGrid()
+                self.changelivingCellColor()
+
+            self.golGrid.stepGridForward()
+
             time.sleep(self.secondsPerIteration)
 
-            displayGrid = [
-                currentCell for currentSublist in self.golGrid.grid for currentCell in currentSublist]
-            self.sense.set_pixels(displayGrid)
-            currentIteration += 1
+
+    def changelivingCellColor(self):
+        self.livingCellColor[0] = random.randint(0, 255)
+        self.livingCellColor[1] = random.randint(0, 255)
+        self.livingCellColor[2] = random.randint(0, 255)
 
 
 class GOLGrid:
+    isStagnant = False
+
     def __init__(self):
         self.initGrid()
 
     def initGrid(self):
+
         self.grid = []
 
         for currentRowIndex in range(0, 8):
@@ -38,8 +71,14 @@ class GOLGrid:
                 currentRow.append(False)
             self.grid.append(currentRow)
 
+    def randomizeGrid(self):
+        for currentRowIndex in range(0, len(self.grid)):
+            for currentColumnIndex in range(0, len(self.grid[currentRowIndex])):
+                self.grid[currentRowIndex][currentColumnIndex] = (
+                    random.randint(0, 1) == 0)
+
     def stepGridForward(self):
-        bufferGrid = []
+        self.bufferGrid = []
         for currentRowIndex in range(0, 8):
             currentBufferRow = []
             for currentColumnIndex in range(0, 8):
@@ -56,8 +95,10 @@ class GOLGrid:
                         currentBufferRow.append(False)
                     else:
                         currentBufferRow.append(True)
-            bufferGrid.append(currentBufferRow)
-        self.grid = bufferGrid
+            self.bufferGrid.append(currentBufferRow)
+
+        self.isStagnant = (self.grid == self.bufferGrid)
+        self.grid = self.bufferGrid
 
     def getTotalLivingNeighborsOfTargetCell(self, targetCellRow, targetCellColumn):
         totalLivingNeighbors = 0
@@ -87,3 +128,7 @@ class GOLGrid:
             totalLivingNeighbors += 1
 
         return totalLivingNeighbors
+
+
+senseHatGOL = SenseHatGOL(0.5, 120)
+senseHatGOL.execute()
